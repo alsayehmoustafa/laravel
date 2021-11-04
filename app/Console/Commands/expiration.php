50464164d020f26eeeabafb7cad3ca511dfdc06a
Moustafa Alsayeh <moustafaalsayeh@icloud.com>
2021-11-05 01:40:11 +0200

    Expiration Feature

---
+++ app/Console/Commands/expiration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class expiration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:expire';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Expire user session every five minutes.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return Command::SUCCESS;
+    }
+}
